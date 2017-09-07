a83d52eccf36edbfd08bed13ca94f828aee4c671
jlperla <jesseperla@gmail.com>
2017-09-07 10:18:07 -0700

    Added example differentiating a linear system.

---
+++ examples/MAD/differentiating_linear_system.m
@@ -0,0 +1,29 @@
+%This example shows auto-differentiation of a linear system
+%We have no idea on the performance of this process, but it is a proof of concept.
+
+madinitglobals;
+a_val = 1.0; %Seeding `a' with the value of 1.
+a = fmad(a_val, 1); %Creates an `a` variable
+
+%Now, solve the system [1 0; 0 a] [x_1; x_2] = b for [x_1; x_2] using the matlab left multip
+b = [.1; .2]; %right hand side.
+
+%First just using the number,
+[1 0; 0 a_val] \ b
+
+%But this can also be auto-differentiated!  Note the derivatives.
+[1 0; 0 a] \ b
+
+%Can also do much crazier setups, such as [1 0; 0 a] [x_1; x_2] = [.1; .2]
+a_val = 1.4
+a = fmad(a_val, 1); %Creates an `a` variable
+[a^2 0; 0 a] \ b
+
+%For sparse systems:
+sparse_b = sparse(b);
+A_sparse = sparse([1;2],[1;2],[1;a]);
+%Display in full form, including derivatives with respect to `a`
+full(A_sparse)
+
+%Solve the system and differentiation
+A_sparse \ b
---
+++ examples/README.md
@@ -0,0 +1,10 @@
+# Examples
+This contains MAD and tomlab examples
+
+
+
+## MAD
+
+* [MAD/differentiating_linear_system.m](MAD/differentiating_linear_system.m): Uses AD of a linear system with the matlab `A \ B` syntax.  Not sure of performance
+
+
